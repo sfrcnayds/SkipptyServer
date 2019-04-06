1d24426c1cfe201bbd2b55885c54c563141a48d3
Sefercan AYDAS <sfrcnayds@gmail.com>
2019-04-06 14:17:37 +0300

    Initial Commit

---
+++ src/skipptyserver/SkipptyServer.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package skipptyserver;
+
+
+/**
+ *
+ * @author INSECT
+ */
+public class SkipptyServer {
+
+
+    public static void main(String[] args) {
+        Server.Start(2000);
+    }
+
+}
